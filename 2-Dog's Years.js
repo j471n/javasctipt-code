269e582cffd9768f07772f71b1ce33038132d7e9
Jatin sharma <jatinsharma089659@gmail.com>
2020-09-05 22:40:46 +0530

    Create 2-Dog's Years.js

---
+++ 2-Dog's Years.js
@@ -0,0 +1,19 @@
+//link - https://www.codecademy.com/courses/introduction-to-javascript/projects/dog-years-javascript
+
+
+//my age
+var myAge = 18;
+
+//early years
+var earlyYears = 2;
+
+earlyYears *=10.5;
+
+//minus my age
+var laterYears = myAge -2;
+
+laterYears *=4
+let myAgeInDogYears = earlyYears + laterYears;
+let myName = "Jatin".toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
